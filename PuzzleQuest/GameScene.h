4ebb1355a2633253d7597ca921bf09ccb2f04318
Ken Zheng <kzheng@Kens-iMac.local>
2015-06-22 20:37:15 -0700

    initial commit
    Signed-off-by: Ken Zheng <kzheng@Kens-iMac.local>

---
+++ PuzzleQuest/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  PuzzleQuest
+//
+
+//  Copyright (c) 2015 Ken Zheng. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ PuzzleQuest/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  PuzzleQuest
+//
+
+//  Copyright (c) 2015 Ken Zheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
