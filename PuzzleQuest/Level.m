ab279edb89feb6bf059810cdd238348b1d61d70f
Ken Zheng <kzheng@Kens-iMac.local>
2015-06-25 11:49:12 -0700

    all possible swap when the game starts is now recorded

--- PuzzleQuest/Level.m
+++ PuzzleQuest/Level.m
@@ -11,6 +11,10 @@
 #import "Tile.h"
 #import "Swap.h"
 
+@interface Level ()
+@property (nonatomic, strong) NSSet *possibleSwaps;
+@end
+
 @implementation Level {
     Cookie *_cookies[NumColumns][NumRows];  // keeps track of Cookie objects
     Tile *_tiles[NumColumns][NumRows];      // keeps track of which tiles are empty or contain a Cookie
@@ -29,7 +33,16 @@
 }
 
 - (NSSet *)shuffle {
-    return [self createInitialCookies];
+    NSSet *set;
+    do {
+        set = [self createInitialCookies];
+        
+        [self detectPossibleSwaps];
+        
+        NSLog(@"possible swaps: %@", self.possibleSwaps);
+    } while ([self.possibleSwaps count] == 0);
+    
+    return set;
 }
 
 - (Cookie *)cookieAtColumn:(NSInteger)column row:(NSInteger)row {
@@ -96,6 +109,79 @@
     }
     
     return set;
+}
+
+// this steps thru the grid and swap each cookie with the one next to it, once horizontally and then vertically
+- (void)detectPossibleSwaps {
+    NSMutableSet *set = [NSMutableSet set];
+    
+    for (NSInteger row = 0; row < NumRows; row++) {
+        for (NSInteger column = 0; column < NumColumns; column++) {
+            Cookie *cookie = _cookies[column][row];
+            if (cookie != nil) {
+                if (column < NumColumns - 1) {  // is it possible to swap this cookie with the one on the right?
+                    Cookie *otherCookie = _cookies[column + 1][row];
+                    if (otherCookie != nil) {
+                        // swap them
+                        _cookies[column][row] = otherCookie;
+                        _cookies[column + 1][row] = cookie;
+                        
+                        // is either cookie part of a chain?
+                        if ([self hasChainAtColumn:column + 1 row:row] ||
+                            [self hasChainAtColumn:column row:row]) {
+                            
+                            Swap *swap = [[Swap alloc] init];
+                            swap.cookieA = cookie;
+                            swap.cookieB = otherCookie;
+                            [set addObject:swap];
+                        }
+                        
+                        // swap them back
+                        _cookies[column][row] = cookie;
+                        _cookies[column + 1][row] = otherCookie;
+                    }
+                }
+                
+                if (row < NumRows - 1) {
+                    Cookie *otherCookie = _cookies[column][row + 1];
+                    if (otherCookie != nil) {
+                        // swap them
+                        _cookies[column][row] = otherCookie;
+                        _cookies[column][row + 1] = cookie;
+                        
+                        if ([self hasChainAtColumn:column row:row + 1] ||
+                            [self hasChainAtColumn:column row:row]) {
+                            
+                            Swap *swap = [[Swap alloc] init];
+                            swap.cookieA = cookie;
+                            swap.cookieB = otherCookie;
+                        }
+                        
+                        // swap them back
+                        _cookies[column][row] = cookie;
+                        _cookies[column][row + 1] = otherCookie;
+                    }
+                }
+            }
+        }
+    }
+    
+    self.possibleSwaps = set;
+}
+
+// a chain is 3 or more consecutive cookies of the same type in a row or column
+- (BOOL)hasChainAtColumn:(NSInteger)column row:(NSInteger)row {
+    NSUInteger cookieType = ((Cookie *)_cookies[column][row]).cookieType;
+    
+    NSUInteger horizontalLength = 1;
+    for (NSInteger i = column - 1; i >= 0 && _cookies[i][row].cookieType == cookieType; i--, horizontalLength++);
+    for (NSInteger i = column + 1; i < NumColumns && _cookies[i][row].cookieType == cookieType; i++, horizontalLength++);
+    if (horizontalLength >= 3)  return YES;
+    
+    NSUInteger verticalLength = 1;
+    for (NSInteger i = row - 1; i >= 0 && _cookies[column][i].cookieType == cookieType; i--, verticalLength++);
+    for (NSInteger i = row + 1; i < NumRows && _cookies[column][i].cookieType == cookieType; i++, verticalLength++);
+    return (verticalLength >= 3);
 }
 
 @end
